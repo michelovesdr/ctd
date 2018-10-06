fef8fe8b5032f24e2f0aca6552b877053e98ccd8
Michelle Bocanegra <michelovesdr@gmail.com>
2018-10-06 15:56:48 +0000

    made changes to clothes class

--- week_three/class_practice/clothes.rb
+++ week_three/class_practice/clothes.rb
@@ -1,38 +1,38 @@
 #Master Class is clothes
-class clothes
-    def initiate (cover your body)
+class Clothes
+    def initiate #cover your body
     end
-    def dresses (are usually flowy)
+    def dresses #are usually flowy
     end
-    dresses = [skater, peplum, sheath, shift, bandage, aline, tulle]
+class Dresses = ["skater", "peplum", "sheath", "shift", "bandage", "aline", "tulle"]
 end
     def pants (long pants)
     end
-    pants = [dress pants, jeans, culottes, haram, bell, leggings]
+class Pants = ["dress pants", "jeans", "culottes", "haram", "bell", "leggings"]
 end
     def shirts
     end
-    shirts = [blouses, sailor, henley, polo, tunic, jersey, halter, strapless, vneck, surplice, turtleneck]
+class Shirts = ["blouses", "sailor", "henley", "polo", "tunic", "jersey", "halter", "strapless", "vneck", "surplice", "turtleneck"]
 end
     def shorts
     end
-    shorts = [tennis, running, cargo, pleated, denim, bermuda, skort, booty]
+class Shorts = ["tennis", "running", "cargo", "pleated", "denim", "bermuda", "skort", "booty"]
 end
     def socks
     end
-    socks = [ankle, crew, noshow, knee length, thighhighs, mid calf, calf]
+class Socks = ["ankle", "crew", "noshow", "knee length", "thighhighs", "mid calf", "calf"]
 end
     def underwear
     end
-    underwear = [briefs, bikini, boyshorts, hiphuggers, boxers]
+class Underwear = ["briefs", "bikini", "boyshorts", "hiphuggers", "boxers"]
 end
     def skirts
     end
-    skirts = [layered, pencil, peasant, maxi, mini, midi, poodle, tulip]
+class Skirts = ["layered", "pencil", "peasant", "maxi", "mini", "midi", "poodle", "tulip"]
 end
     def bras
     end
-    bras = [pushup, tshirt, convertible, balconette, bandeau, bralette, sports, plunge, cami]
+class Bras = ["pushup", "tshirt", "convertible", "balconette", "bandeau", "bralette", "sports", "plunge", "cami"]
 end
 
 end
